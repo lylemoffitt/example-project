3a5c36469acc58cd9be53a621fb08653de8d49df
Lyle Moffitt <lyle.moffitt@cyphyworks.com>
2019-01-21 17:37:15 -0500

    Fixup cmake. Add debug/release compile options

--- test/src/test_libPrinter.cpp
+++ test/src/test_libPrinter.cpp
@@ -4,14 +4,28 @@
 
 #include <sstream>
 
+#ifndef APPEND_ENDLINE
+#define APPEND_ENDLINE 1
+#endif
+
+#if APPEND_ENDLINE
+#define NL_STR "\n"
+#else
+#define NL_STR ""
+#endif
+
 namespace {
+
+
 
 TEST(HelloTest, print_hello_get_hello) {
     std::stringstream ss;
     Printer printer(ss);
     printer.print("hello");
 
-    EXPECT_STREQ("hello\n", ss.str().c_str());
+    const char expected[] = "hello" NL_STR;
+
+    EXPECT_STREQ(expected, ss.str().c_str());
 }
 
 } // namespace

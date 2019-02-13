699bbc099832d4c010034e70865731a0fc63ebcb
Lyle Moffitt <lyle.moffitt@cyphyworks.com>
2019-02-12 19:04:15 -0500

    Set git version block to update with every commit

--- code/libPrinter/cmake/GitVersionInfo.cmake
+++ code/libPrinter/cmake/GitVersionInfo.cmake
@@ -1,40 +1,86 @@
-# Get git hash and description for current commit
-execute_process(
-    COMMAND git rev-parse HEAD
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    OUTPUT_VARIABLE GIT_HASH
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-execute_process(
-    COMMAND git describe
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    OUTPUT_VARIABLE GIT_DESC
-    ERROR_QUIET
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-execute_process(
-    COMMAND git remote
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    OUTPUT_VARIABLE GIT_REMOTE
-    ERROR_QUIET
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-execute_process(
-    COMMAND git remote get-url ${GIT_REMOTE}
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    OUTPUT_VARIABLE GIT_REMOTE_URL
-    ERROR_QUIET
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-execute_process(
-    COMMAND git rev-parse --abbrev-ref HEAD
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    OUTPUT_VARIABLE GIT_BRANCH
-    ERROR_QUIET
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-execute_process(
-    COMMAND git diff --quiet
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    RESULT_VARIABLE GIT_DIRTY
-)
+
+include(GetGitRevisionDescription)
+
+
+function(git_commit_hash _out_var_)
+    get_git_head_revision(refspec hash)
+    if(NOT hash)
+        set(${_var} "HEAD-HASH-NOTFOUND" PARENT_SCOPE)
+        return()
+    endif()
+    execute_process(
+        COMMAND git rev-parse ${hash}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        OUTPUT_VARIABLE stdout
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    set(${_out_var_} "${stdout}" PARENT_SCOPE)
+endfunction(git_commit_hash)
+
+
+function(git_describe _out_var_)
+    get_git_head_revision(refspec hash)
+    if(NOT hash)
+        set(${_var} "HEAD-HASH-NOTFOUND" PARENT_SCOPE)
+        return()
+    endif()
+    execute_process(
+        COMMAND git describe ${hash}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        OUTPUT_VARIABLE stdout
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    set(${_out_var_} "${stdout}" PARENT_SCOPE)
+endfunction(git_describe)
+
+
+function(git_remote_url _out_var_)
+    execute_process(
+        COMMAND git remote
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        OUTPUT_VARIABLE GIT_REMOTE
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    execute_process(
+        COMMAND git remote get-url ${GIT_REMOTE}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        OUTPUT_VARIABLE stdout
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    set(${_out_var_} "${stdout}" PARENT_SCOPE)
+endfunction(git_remote_url)
+
+
+function(git_current_branch _out_var_)
+    get_git_head_revision(refspec hash)
+    if(NOT hash)
+        set(${_var} "HEAD-HASH-NOTFOUND" PARENT_SCOPE)
+        return()
+    endif()
+    execute_process(
+        COMMAND git rev-parse --abbrev-ref ${hash}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        OUTPUT_VARIABLE stdout
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    set(${_out_var_} "${stdout}" PARENT_SCOPE)
+endfunction(git_current_branch)
+
+
+function(git_local_changes _out_var_)
+    get_git_head_revision(refspec hash)
+    if(NOT hash)
+        set(${_var} "HEAD-HASH-NOTFOUND" PARENT_SCOPE)
+        return()
+    endif()
+    execute_process(
+        COMMAND git diff --quiet ${hash}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        RESULT_VARIABLE res
+    )
+    set(${_out_var_} ${res} PARENT_SCOPE)
+endfunction(git_local_changes)
